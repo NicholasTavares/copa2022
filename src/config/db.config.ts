31e038fd1de6c86173bd73f077d69bc38930a9af
NicholasTavares <nicholasbalby@hotmail.com>
2022-09-16 20:38:23 -0300

    refactor: change database to postgres

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,15 +2,15 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => {
   return {
-    type: 'mysql',
+    type: 'postgres',
     logging: true,
     host: process.env.DB_MAIN_HOST || 'localhost',
-    port: process.env.DB_MAIN_PORT || 3306,
-    username: process.env.DB_MAIN_USER || 'root',
-    password: process.env.DB_MAIN_PASSWORD || 'westeros',
-    database: process.env.DB_MAIN_DATABASE || 'copa2022',
+    port: process.env.DB_MAIN_PORT || 5433,
+    username: process.env.DB_MAIN_USER || 'postgres',
+    password: process.env.DB_MAIN_PASSWORD || 'postgres',
+    database: process.env.DB_MAIN_DATABASE || 'copa',
     autoLoadEntities: true,
-    //synchronize: true,
+    synchronize: true,
     entities: ['dist/src/**/entities/*.entity{.ts,.js}'],
     migrations: ['../shared/migrations/*{.ts,.js}'],
     cli: {
